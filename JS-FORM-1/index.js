1f6a050bc209489e1f88110e2fdbf85d76a5712f
cmouns <mounir.sebti33@gmail.com>
2025-06-30 14:44:45 +0200

    new

--- JS-FORM-1/index.js
+++ JS-FORM-1/index.js
@@ -1,20 +1,54 @@
+// Calculatrice
+// let firstNumber = document.getElementById('firstNumber')
+// let secondNumber = document.getElementById('secondNumber')
+// let result = document.getElementById('result')
+// let myBouton = document.getElementById('myButton')
 
-let firstNumber = document.getElementById('firstNumber')
-let secondNumber = document.getElementById('secondNumber')
-let result = document.getElementById('result')
-let myBouton = document.getElementById('myButton')
 
 
 
-firstNumber.addEventListener('change', () =>{
-    firstNumbertest = firstNumber.value
+
+// myBouton.addEventListener('click', () =>{
+
+
+// var nombre1 = parseInt(firstNumber.value)
+// var nombre2 = parseInt(secondNumber.value)
+// result.value= nombre1 + nombre2
+// })
+
+
+// ---------- EXO AFFICHER/CACHER
+// var element = document.getElementById('second');
+// element.classList.remove('hidden')
+
+//CACHER LES SPOILERS
+// var elements = document.getElementsByClassName('spoiler');
+
+// let bouton = document.getElementById('spoil')
+
+// bouton.addEventListener('click', () =>{
+// for (var i = 0; i < elements.length; i++) {
+//   var element = elements[i];
+//   element.classList.add('hidden');
+// }
+// console.log(element)
+
+// })
+
+
+var elements = document.getElementsByTagName("p")
+var button = document.getElementById('button');
+
+for(let i=0; i<elements.length;i++){
+elements[i].addEventListener('click', (e) =>{ 
+e.target.style.backgroundColor = "yellow";
 })
-secondNumber.addEventListener('change', () =>{
-    secondNumbertest = secondNumber.value
+}
+
+button.addEventListener('click', () =>{
+for(let i=0; i<elements.length;i++){
+    elements[i].style.backgroundColor = "yellow";
+}
 })
 
-myBouton.addEventListener('click', () =>{
 
-    result.value= parseInt(firstNumbertest) + parseInt(secondNumbertest)
-    console.log(firstNumbertest +" "+ secondNumbertest)
-})
